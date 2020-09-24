3fe5de307f89a95b2030b86665de82f37e7ea20e
Toktam <ttmohebbi@gmail.com>
2020-09-24 17:25:12 +0000

    added some of the edge cases and included file siza

--- fetcher.js
+++ fetcher.js
@@ -5,21 +5,35 @@ const fs = require('fs');
 const url = process.argv[2];
 // console.log(url);
 
-const location = process.argv[3];
-// console.log(location);
+const localPath = process.argv[3];
+// console.log(localPath);
 
-const fetcher = function(url, location) {
+const fetcher = function(url, localPath) {
   request(url, (err, res, body) => {
-
+    if (err) {
+      console.log(err);
+    }
+    //console.log res and statusCode here
     const data = new Uint8Array(Buffer.from(body));
-
-    fs.writeFile(location, data, (err) => {
-      if (err) throw err;
-      console.log(`Downloaded and saved the url to ${location}`);
+    // console.log("data is: ", data);
+    fs.writeFile(localPath, body, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`Downloaded and saved saved ${body.length} bytes to ${localPath}`);
+      }
     });
 
   })
 
 }
 
-fetcher(url, location);
+if (!localPath) {
+  console.log('please provide a valid local path!')
+} else if (!url) {
+  console.log('please provide a valid url!');
+} else if (url && localPath) {
+  fetcher(url, localPath);
+}
+
+
